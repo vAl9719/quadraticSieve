6d09542ad2520c1468cc99afaafdf5803166f153
vAl9719 <alyssa.zhao5@gmail.com>
2022-03-30 17:24:31 -0400

    part 3 file started

---
+++ step3.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+from sympy import *
+
+# Alyssa - Use mod 2 linear algebra (Gaussian elimination) on exponents to find products of these x’s that is a square (zero vector)
+# If a = +-b (mod n), try again
+# Find more smooth values of x^2 - n, find a new linear dependency - how?
+# Otherwise, take gcd(a - b, n) to get a nontrivial factor
