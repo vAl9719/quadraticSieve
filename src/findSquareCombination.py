af9fdbf33d85dd749bbb840a8634abf86cddd033
vAl9719 <alyssa.zhao5@gmail.com>
2022-04-10 17:05:01 -0400

    tried implementing a way to find x and y

--- src/findSquareCombination.py
+++ src/findSquareCombination.py
@@ -17,6 +17,81 @@ def clear_zeros(A):
     # remove rows having all zeroes
     A = A[~np.all(A == 0, axis=1)]
 
+# input M_null: the nullspace of the matrix
+# output: vec, the current combination of potential values where a 
+# 1 represents which candidates are being used
+# and indices, an array of indices of candidates in the nullspace
+def setArray(nullspace):
+    v = []
+    
+    #translate nullspace to an array for easier use
+    for i in range(0, len(nullspace[0])):
+        if nullspace[0][i] != 0:
+            v.append(1)
+        else:
+            v.append(0)
+    
+    #sum of 1s in the array
+    sum = np.sum(v)
+    #vec represents the current combination 
+    vec = np.zeros(sum, dtype = 'int')
+    vec[sum-1] = 1 #here it's just only the last index 
+    
+    #set up the indices of candidates in the nullspace 
+    indices = []
+    for i in range(0, len(nullspace[0])):
+        if(v[i] == 1):
+            indices.append(i)
+    
+    print(indices)
+            
+    return [vec, indices]
+
+# input: vec, the current combination vector from the candidates
+# output: a new vec, the new combination vector
+# if no more combinations can be found, then the program quits
+def findCombs(vec):
+    index = len(vec) - 1
+    while(vec[index] == 1):
+        vec[index] = 0
+        index = index-1
+    
+    if(index >= 0):
+        vec[index] = 1
+    
+    else:
+        print("No combinations")
+        quit()
+
+    return vec    
+
+# input: the RREF matrix, the current combination, and the indices
+# output: a full-sized solution vector compute used to calculate x and y
+def determineSolVector(M_rref, vec, indices):
+
+    M_rref = np.array(M_rref)
+    sol = np.zeros(len(M_rref[0]), dtype= 'int')
+    
+    for i in range(0, len(vec)):
+        if(vec[i] == 1):
+            sol[indices[i]] = 1
+        else:
+            sol[indices[i]] = 2
+            
+    for i in reversed(range(0, len(M_rref))):
+        sum = 0
+        for j in range(0, len(M_rref[0])):
+            sum = sum + sol[j]*M_rref[i][j]
+        zero = (np.argwhere(sol == 0).flatten())
+        sum = sum % 2
+        if(len(zero) != 0):
+            index = zero[len(zero)-1]
+            if(sum == 0):
+                sol[index] = 2
+            else:
+                sol[index] = 1
+    
+    return sol%2    
 
 # input BSmoothList: a 2D array where [x][y] represents the exponent of the yth prime factor of x (b-smooth)
 # output nullspace: the nullspace of BSmoothList, AKA a subsequence of b smooth numbers that has a product square
@@ -49,10 +124,12 @@ def findSquareCombination(BSmoothList):
     for i in range (0,len(nullspace)): 
         nullspace[i] = nullspace[i]%2
 
-    rows = len(BSmoothList_rref)
-    cols = len(BSmoothList_rref[0])
+    # calc initial vectors
 
-    return nullspace
+    vec, indices = setArray(nullspace)
+    sol = determineSolVector(BSmoothList_rref, vec, indices)
+
+    return vec, indices, sol, nullspace, BSmoothList_rref
 
     #solution_nums = [smooth_nums[i] for i in solution_vec]
     #idea is that if nullspace != 0 at index i, then smooth_nums[i] is a potential solution
--- src/quadraticSieve.py
+++ src/quadraticSieve.py
@@ -1,7 +1,7 @@
 from tkinter import YView
 from factorBaseB import findOptimalB, factorBase
-from findBSmoothValues import findBSmoothValues
-from findSquareCombination import findSquareCombination
+from findBSmoothValues import findBSmoothValues, factorize
+from findSquareCombination import *
 import util
 
 '''
@@ -10,16 +10,76 @@ Main quadratic sieve algorithm
 
 
 def quadraticSieve(n: int):
-    B = findOptimalB(n)
+    B = findOptimalB(n) 
     factorBaseValues = factorBase(B)
     candidates, squaredFactorizations = findBSmoothValues(n, factorBaseValues)
-    while True:
-        nullspace = findSquareCombination(squaredFactorizations)
-        a, b = findAB(candidates, nullspace)
-        if not inversesModN(a, b, n):
-            return gcd(a - b, n)
+    vec, indices, sol, nullspace, BSmoothList_rref = findSquareCombination(squaredFactorizations)
+    possible = findPossible(candidates, nullspace)
 
+    #initial values
+    x = multiplyAll(v, possible)
+    xsq = x*x % n
+    ysq = factorize(xsq, factorBaseValues)
+    y = squareRoot(ysq)
 
+    while inversesModN(x, y, n):
+        # x and y are congruent, find a different combination
+        vec = findCombs(vec)
+        v = determineSolVector(BSmoothList_rref, vec, indices)
+        xsq = x*x % n
+        ysq = factorize(xsq, factorBase)
+        y = squareRoot(ysq)
+    
+    print("Found x: ", x)
+    print("Found y: ", y)
+
+    factor1 = gcd(math.abs(x-y), n)
+    factor2 = n/factor1
+
+    print("First factor: ", factor1)
+    print("Second factor: ", factor2)
+
+    return factor1, factor2 
+
+# input: v, a vector of 1s and 0s, and a list of nums to multiply
+# output: the product
+def multiplyAll(v, nums):
+    product = 1
+    for i in range(0, len(v)):
+        if v[i] == 0:
+            continue
+        product=  product * nums[i]
+    
+    return product
+
+#input: a number to be square rooted
+#output: the square root of a number
+# code comes from: https://riptutorial.com/python/example/8751/computing-large-integer-roots
+# but the second param is just 2
+def squareRoot(num):
+    upper_bound = 1
+    while upper_bound ** 2 <= num:
+        upper_bound *= 2
+    
+    lower_bound = upper_bound // 2
+
+    while lower_bound < upper_bound:
+        mid = (lower_bound + upper_bound) // 2
+        mid_nth = mid ** 2
+        if lower_bound < mid and mid_nth < num:
+            lower_bound = mid
+        elif upper_bound > mid and mid_nth > num:
+            upper_bound = mid
+        else:
+            # Found perfect nth root.
+            return mid
+    return mid + 1    
+
+def findPossible(candidates, nullspace):
+    possible = []
+    for i in range(0, len(nullspace[0])):
+        if nullspace[0][i] != 0:
+            possible.append(candidates[i])
 
 def findAB(candidateList, nullspace): 
     return 0, 0
